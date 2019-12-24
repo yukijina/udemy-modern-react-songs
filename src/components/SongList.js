9a9e6f9464d5aa8e55d5bcf0d913506f191d41c5
yukijina <yukijina@gmail.com>
2019-12-23 21:15:15 -0800

    display songs and connect store

---
+++ src/components/SongList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions/index';
+
+class SongList extends React.Component {
+    renderedList() {
+        return this.props.songs.map(song => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button 
+                        className="ui button primary" 
+                        onClick={() => this.props.selectSong(song)}
+                        >
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            )
+        })
+    }
+    render() {
+        return(
+            <div className="ui devided list">
+                {this.renderedList()}
+            </div>
+        )
+    }    
+}
+
+const mapStateToProps = state => {
+    return {
+        songs: state.songs
+    }
+}
+
+export default connect(mapStateToProps, { selectSong })(SongList);
